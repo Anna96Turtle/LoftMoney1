70c46547713d440f80fa87201e54cdad25a9b9c1
Anna <anna2.00@mail.ru>
2020-03-29 20:07:14 +0300

    DZ #3

---
+++ app/src/main/java/com/loftblog/loftmoney/skreens/AddItemActivity.java
@@ -0,0 +1,17 @@
+package com.loftblog.loftmoney.skreens;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.loftblog.loftmoney.R;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
